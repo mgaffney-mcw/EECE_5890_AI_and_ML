c3744afe2690362be4c6d4f78694347fe2ad3f69
mgaffney-mcw <49734151+mgaffney-mcw@users.noreply.github.com>
2024-03-29 11:59:21 -0500

    EECE_5890_Hwk4 - Removing stop words from abstract column

--- EECE_5890_Hwk4.py
+++ EECE_5890_Hwk4.py
@@ -19,8 +19,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from tkinter import Tk, filedialog
-import seaborn as sns
-from scipy.stats import pearsonr, spearmanr
+import nltk
+#nltk.download()
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 # User selects dataset from file
 root = Tk()
@@ -34,6 +36,21 @@ if not fName:
 dataset=pd.read_csv(fName)
 print(' ')
 print('Dataset loaded.')
+
+# Removing stop words from the abstract column
+stp_wrds = set(stopwords.words('english'))
+
+# Pre-allocating things
+abs_no_stop = np.empty((len(dataset.abstract), 1))
+abs_no_stop[:] = np.nan
+abs_no_stop = pd.DataFrame(abs_no_stop, columns=['abstract'])
+
+count = 0
+for i in dataset.abstract:
+    i_tkn = word_tokenize(i)
+    abs_no_stop.loc[count] = [[w for w in i_tkn if not w.lower() in stp_wrds]]
+    count = count + 1
+
 
 
 print('Done!')
