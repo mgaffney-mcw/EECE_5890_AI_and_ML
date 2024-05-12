39347bb3b6643861c031c2141455a07ff95be1a7
mgaffney-mcw <49734151+mgaffney-mcw@users.noreply.github.com>
2024-05-12 00:23:39 -0500

    more EECE term project work

--- EECE_5890_TermProject.py
+++ EECE_5890_TermProject.py
@@ -12,6 +12,7 @@ from sklearn.linear_model import LinearRegression
 
 from tensorflow.keras import layers
 from tensorflow.keras import Model
+import tensorflow as tf
 
 from pathlib import Path
 import os
@@ -31,9 +32,10 @@ if not fName:
     quit()
 
 # Reading in dataset as dataframe
-dataset=pd.read_excel(fName, sheet_name=['AOIP','OCVL'])
-AOIP_df=dataset['AOIP']
-OCVL_df=dataset['OCVL']
+#dataset=pd.read_excel(fName, sheet_name=['AOIP','OCVL'])
+dataset=pd.read_excel(fName)
+AOIP_df=dataset
+#OCVL_df=dataset['OCVL']
 
 print(' ')
 print('Dataset loaded.')
@@ -72,50 +74,52 @@ augpath_rawavi_split = Path.joinpath(augpath_rawavi, "split")
 # Checking for duplicate filenames in the Confocal and Split columns
 duplicate_AOIP_Conf = AOIP_df['Confocal Image name'].duplicated()
 duplicate_AOIP_Split = AOIP_df['Split Image name'].duplicated()
-duplicate_OCVL_Conf = OCVL_df['Confocal Image name'].duplicated()
-duplicate_OCVL_Split = OCVL_df['Split Image name'].duplicated()
+#duplicate_OCVL_Conf = OCVL_df['Confocal Image name'].duplicated()
+#duplicate_OCVL_Split = OCVL_df['Split Image name'].duplicated()
 
 # Deleting any duplicate values
-cleaned_AOIP_df = AOIP_df.drop_duplicates(subset=['Confocal Image name'], keep='first')
-cleaned_AOIP_df = AOIP_df.drop_duplicates(subset=['Split Image name'], keep='first')
+# cleaned_AOIP_df = AOIP_df.drop_duplicates(subset=['Confocal Image name'], keep='first')
+# cleaned_AOIP_df = AOIP_df.drop_duplicates(subset=['Split Image name'], keep='first')
 
-cleaned_OCVL_df = OCVL_df.drop_duplicates(subset=['Confocal Image name'], keep='first')
-cleaned_OCVL_df = OCVL_df.drop_duplicates(subset=['Split Image name'], keep='first')
+cleaned_AOIP_df = AOIP_df.copy()
+
+#cleaned_OCVL_df = OCVL_df.drop_duplicates(subset=['Confocal Image name'], keep='first')
+#cleaned_OCVL_df = OCVL_df.drop_duplicates(subset=['Split Image name'], keep='first')
 
 # Rounding SNR value and Average to two decimal places
 cleaned_AOIP_df = cleaned_AOIP_df.round({'Confocal SNR value': 2, 'Confocal Average Grade': 2, 'Split SNR value': 2, 'Split Average Grade': 2})
-cleaned_OCVL_df = cleaned_OCVL_df.round({'Confocal SNR value': 2, 'Confocal Average Grade': 2, 'Split SNR value': 2, 'Split Average Grade': 2})
+#cleaned_OCVL_df = cleaned_OCVL_df.round({'Confocal SNR value': 2, 'Confocal Average Grade': 2, 'Split SNR value': 2, 'Split Average Grade': 2})
 
 # Separating out Confocal and Split modalities
 AOIP_confocal = cleaned_AOIP_df[['Confocal Image name','Confocal SNR value','Confocal Grader 1','Confocal Grader 2','Confocal Grader 3','Confocal Average Grade']]
 AOIP_Split = cleaned_AOIP_df[['Split Image name','Split SNR value','Split Grader 1','Split Grader 2','Split Grader 3','Split Average Grade']]
-OCVL_confocal = cleaned_OCVL_df[['Confocal Image name','Confocal SNR value','Confocal Grader 1','Confocal Grader 2','Confocal Grader 3','Confocal Average Grade']]
-OCVL_Split = cleaned_OCVL_df[['Split Image name','Split SNR value','Split Grader 1','Split Grader 2','Split Grader 3','Split Average Grade']]
+#OCVL_confocal = cleaned_OCVL_df[['Confocal Image name','Confocal SNR value','Confocal Grader 1','Confocal Grader 2','Confocal Grader 3','Confocal Average Grade']]
+#OCVL_Split = cleaned_OCVL_df[['Split Image name','Split SNR value','Split Grader 1','Split Grader 2','Split Grader 3','Split Average Grade']]
 
 # Checking that there are no missing values
 AOIP_confocal_notnull = AOIP_confocal[pd.notnull(AOIP_confocal)]
 AOIP_Split_notnull = AOIP_Split[pd.notnull(AOIP_Split)]
-OCVL_confocal_notnull = OCVL_confocal[pd.notnull(OCVL_confocal)]
-OCVL_Split_notnull = OCVL_Split[pd.notnull(OCVL_Split)]
+#OCVL_confocal_notnull = OCVL_confocal[pd.notnull(OCVL_confocal)]
+#OCVL_Split_notnull = OCVL_Split[pd.notnull(OCVL_Split)]
 
 # Summarizing data
 # Showing boxplots of all SNR values
-box_data_SNR = [AOIP_confocal_notnull['Confocal SNR value'], AOIP_Split_notnull['Split SNR value'], OCVL_confocal_notnull['Confocal SNR value'], OCVL_Split_notnull['Split SNR value']]
-box_ticks = ['AOIP Confocal', 'AOIP Split', 'OCVL Confocal', 'OCVL Split']
-fig1, ax1 = plt.subplots()
-ax1.set_title('SNR Values')
-ax1.set_xticklabels(box_ticks)
-plt.ylabel("SNR value")
-ax1.boxplot(box_data_SNR)
+# box_data_SNR = [AOIP_confocal_notnull['Confocal SNR value'], AOIP_Split_notnull['Split SNR value'], OCVL_confocal_notnull['Confocal SNR value'], OCVL_Split_notnull['Split SNR value']]
+# box_ticks = ['AOIP Confocal', 'AOIP Split', 'OCVL Confocal', 'OCVL Split']
+# fig1, ax1 = plt.subplots()
+# ax1.set_title('SNR Values')
+# ax1.set_xticklabels(box_ticks)
+# plt.ylabel("SNR value")
+# ax1.boxplot(box_data_SNR)
 
 
 # Showing boxplot of average Grade
-box_data_AvgGrade = [AOIP_confocal_notnull['Confocal Average Grade'], AOIP_Split_notnull['Split Average Grade'], OCVL_confocal_notnull['Confocal Average Grade'], OCVL_Split_notnull['Split Average Grade']]
-fig2, ax2 = plt.subplots()
-ax2.set_title('Average Grade')
-ax2.set_xticklabels(box_ticks)
-plt.ylabel("Average Grade")
-ax2.boxplot(box_data_AvgGrade)
+# box_data_AvgGrade = [AOIP_confocal_notnull['Confocal Average Grade'], AOIP_Split_notnull['Split Average Grade'], OCVL_confocal_notnull['Confocal Average Grade'], OCVL_Split_notnull['Split Average Grade']]
+# fig2, ax2 = plt.subplots()
+# ax2.set_title('Average Grade')
+# ax2.set_xticklabels(box_ticks)
+# plt.ylabel("Average Grade")
+# ax2.boxplot(box_data_AvgGrade)
 
 
 # Reorganizing data so I can visualize with countplot etc
@@ -124,10 +128,10 @@ AOIP_Split_notnull['Modality'] = 1 #Split modality = 1
 AOIP_confocal_notnull['Location'] = 0 #AOIP imaging location = 0
 AOIP_Split_notnull['Location'] = 0 #AOIP imaging location = 0
 
-OCVL_confocal_notnull['Modality'] = 0 #Confocal modality = 0
-OCVL_Split_notnull['Modality'] = 1 #Split modality = 1
-OCVL_confocal_notnull['Location'] = 1 #OCVL imaging location = 1
-OCVL_Split_notnull['Location'] = 1 #OCVL imaging location = 1
+# OCVL_confocal_notnull['Modality'] = 0 #Confocal modality = 0
+# OCVL_Split_notnull['Modality'] = 1 #Split modality = 1
+# OCVL_confocal_notnull['Location'] = 1 #OCVL imaging location = 1
+# OCVL_Split_notnull['Location'] = 1 #OCVL imaging location = 1
 
 All_comb_df = AOIP_confocal_notnull[['Confocal Image name', 'Confocal SNR value', 'Confocal Grader 1','Confocal Average Grade', 'Modality', 'Location']]
 All_comb_df['Grader'] = 1 #Grader 1 = 1
@@ -153,35 +157,56 @@ AOIP_split_G3_tmp = AOIP_Split_notnull.drop(['Split Grader 1','Split Grader 2'],
 AOIP_split_G3_tmp['Grader'] = 3 #Grader 3 = 3
 AOIP_split_G3_tmp = AOIP_split_G3_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 3':'Grade','Split Average Grade':'Average Grade'})
 
-OCVL_conf_G1_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 2','Confocal Grader 3'], axis=1)
-OCVL_conf_G1_tmp['Grader'] = 1 #Grader 1 = 1
-OCVL_conf_G1_tmp = OCVL_conf_G1_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 1':'Grade','Confocal Average Grade':'Average Grade'})
+# OCVL_conf_G1_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 2','Confocal Grader 3'], axis=1)
+# OCVL_conf_G1_tmp['Grader'] = 1 #Grader 1 = 1
+# OCVL_conf_G1_tmp = OCVL_conf_G1_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 1':'Grade','Confocal Average Grade':'Average Grade'})
+#
+# OCVL_conf_G2_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 1','Confocal Grader 3'], axis=1)
+# OCVL_conf_G2_tmp['Grader'] = 2 #Grader 2 = 2
+# OCVL_conf_G2_tmp = OCVL_conf_G2_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 2':'Grade','Confocal Average Grade':'Average Grade'})
+#
+# OCVL_conf_G3_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 1','Confocal Grader 2'], axis=1)
+# OCVL_conf_G3_tmp['Grader'] = 3 #Grader 3 = 3
+# OCVL_conf_G3_tmp = OCVL_conf_G3_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 3':'Grade','Confocal Average Grade':'Average Grade'})
+#
+# OCVL_split_G1_tmp = OCVL_Split_notnull.drop(['Split Grader 2','Split Grader 3'], axis=1)
+# OCVL_split_G1_tmp['Grader'] = 1 #Grader 1 = 1
+# OCVL_split_G1_tmp = OCVL_split_G1_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 1':'Grade','Split Average Grade':'Average Grade'})
+#
+# OCVL_split_G2_tmp = OCVL_Split_notnull.drop(['Split Grader 1','Split Grader 3'], axis=1)
+# OCVL_split_G2_tmp['Grader'] = 2 #Grader 2 = 2
+# OCVL_split_G2_tmp = OCVL_split_G2_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 2':'Grade','Split Average Grade':'Average Grade'})
+#
+# OCVL_split_G3_tmp = OCVL_Split_notnull.drop(['Split Grader 1','Split Grader 2'], axis=1)
+# OCVL_split_G3_tmp['Grader'] = 3 #Grader 3 = 3
+# OCVL_split_G3_tmp = OCVL_split_G3_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 3':'Grade','Split Average Grade':'Average Grade'})
 
-OCVL_conf_G2_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 1','Confocal Grader 3'], axis=1)
-OCVL_conf_G2_tmp['Grader'] = 2 #Grader 2 = 2
-OCVL_conf_G2_tmp = OCVL_conf_G2_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 2':'Grade','Confocal Average Grade':'Average Grade'})
+All_comb_df = pd.concat([All_comb_df, AOIP_conf_G2_tmp, AOIP_conf_G3_tmp, AOIP_split_G1_tmp, AOIP_split_G2_tmp, AOIP_split_G3_tmp], ignore_index=True)
+ #OCVL_conf_G1_tmp, OCVL_conf_G2_tmp, OCVL_conf_G3_tmp, OCVL_split_G1_tmp, OCVL_split_G2_tmp, OCVL_split_G3_tmp], ignore_index=True)
 
-OCVL_conf_G3_tmp = OCVL_confocal_notnull.drop(['Confocal Grader 1','Confocal Grader 2'], axis=1)
-OCVL_conf_G3_tmp['Grader'] = 3 #Grader 3 = 3
-OCVL_conf_G3_tmp = OCVL_conf_G3_tmp.rename(columns={'Confocal Image name':'Image Name', 'Confocal SNR value':'SNR Val','Confocal Grader 3':'Grade','Confocal Average Grade':'Average Grade'})
+# for now, we are just going to round the average grade and use that as our label
+# TODO restructure image data frame to be able to use the All_comb_df for labels
 
-OCVL_split_G1_tmp = OCVL_Split_notnull.drop(['Split Grader 2','Split Grader 3'], axis=1)
-OCVL_split_G1_tmp['Grader'] = 1 #Grader 1 = 1
-OCVL_split_G1_tmp = OCVL_split_G1_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 1':'Grade','Split Average Grade':'Average Grade'})
+AOIP_conf_avg_label = AOIP_confocal_notnull['Confocal Average Grade']
+#OCVL_conf_avg_label = OCVL_confocal_notnull['Confocal Average Grade']
 
-OCVL_split_G2_tmp = OCVL_Split_notnull.drop(['Split Grader 1','Split Grader 3'], axis=1)
-OCVL_split_G2_tmp['Grader'] = 2 #Grader 2 = 2
-OCVL_split_G2_tmp = OCVL_split_G2_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 2':'Grade','Split Average Grade':'Average Grade'})
+AOIP_split_avg_label = AOIP_Split_notnull['Split Average Grade']
+#OCVL_split_avg_label = OCVL_Split_notnull['Split Average Grade']
 
-OCVL_split_G3_tmp = OCVL_Split_notnull.drop(['Split Grader 1','Split Grader 2'], axis=1)
-OCVL_split_G3_tmp['Grader'] = 3 #Grader 3 = 3
-OCVL_split_G3_tmp = OCVL_split_G3_tmp.rename(columns={'Split Image name':'Image Name', 'Split SNR value':'SNR Val','Split Grader 3':'Grade','Split Average Grade':'Average Grade'})
+# all_conf_labels = pd.concat([AOIP_conf_avg_label, OCVL_conf_avg_label], ignore_index=True)
+# all_conf_labels = round(all_conf_labels).astype(int)
+# all_split_labels = pd.concat([AOIP_split_avg_label, OCVL_split_avg_label], ignore_index=True)
+# all_split_labels = round(all_split_labels).astype(int)
 
-All_comb_df = pd.concat([All_comb_df, AOIP_conf_G2_tmp, AOIP_conf_G3_tmp, AOIP_split_G1_tmp, AOIP_split_G2_tmp, AOIP_split_G3_tmp, OCVL_conf_G1_tmp, OCVL_conf_G2_tmp, OCVL_conf_G3_tmp, OCVL_split_G1_tmp, OCVL_split_G2_tmp, OCVL_split_G3_tmp])
+all_conf_labels = AOIP_conf_avg_label.copy()
+all_conf_labels = round(all_conf_labels).astype(int)
+all_split_labels = AOIP_split_avg_label
+all_split_labels = round(all_split_labels).astype(int)
+
+all_conf_labels = round(all_conf_labels).astype(int)
+all_split_labels = round(all_split_labels).astype(int)
 
 ## Image processing to ensure all images are square, of the same dimension and range from 0-1
-
-
 
 for path in avgimg_subdirs:
     if "confocal" in path.name:
@@ -189,7 +214,9 @@ for path in avgimg_subdirs:
         conf_avgimg_tif = [x for x in path.rglob("*.tif")]
         conf_avgimg = conf_avgimg_png + conf_avgimg_tif
 
-        all_conf_images = np.empty([720, 720, len(conf_avgimg)])
+        #all_conf_images = np.empty([720, 720, len(conf_avgimg)])
+        all_conf_images = np.empty([720, 720])
+
 
         counter = 0
         for p in conf_avgimg:
@@ -255,10 +282,27 @@ for path in avgimg_subdirs:
             counter2 = counter2 + 1
 
 
+## splitting dataset into training vs
 
+# split manually
+all_conf_images = all_conf_images / 255
+all_split_images = all_split_images / 255
 
+## Building model
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(720, 720, 1)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(5)])
 
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
+model.fit(all_conf_images, all_conf_labels, epochs=5)
+
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+#
+# print('\nTest accuracy:', test_acc)
 
 # multi-class classification with Keras
 import pandas
